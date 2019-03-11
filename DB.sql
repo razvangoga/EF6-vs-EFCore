8a78cdbda6db9a9ca02d6a9f9e51e39889896795
Razvan Goga <razvan.goga@gmail.com>
2019-03-11 21:49:54 +0100

    db

---
+++ DB.sql
@@ -0,0 +1,12 @@
+﻿create table GuidQueries
+(
+	Id uniqueidentifier not null
+		constraint GuidQueries_pk
+			primary key nonclustered,
+	ExternalId uniqueidentifier not null
+)
+go
+insert into GuidQueries (Id, ExternalId) values ('00000000-0000-0000-0000-000000000001','00000000-0000-0000-0000-00000000000A')
+go
+insert into GuidQueries (Id, ExternalId) values ('00000000-0000-0000-0000-000000000002','00000000-0000-0000-0000-00000000000B')
+go
